71986bd274f9063fab38925d18776906d0270d78
Edmundo Alvarez <edmundoalvarezok@gmail.com>
2025-01-27 18:48:20 -0300

    Se mejoro el presupuesto

--- src/views/Budgets/BudgetDetails.jsx
+++ src/views/Budgets/BudgetDetails.jsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { getBudgetById } from "../../index.js";
 import { Grid } from "react-loader-spinner";
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
+
 function BudgetDetails() {
     const [budget, setBudget] = useState({});
     const [loader, setLoader] = useState(true);
@@ -34,6 +37,101 @@ function BudgetDetails() {
         }).format(value);
     };
 
+    const handleGeneratePDF = async () => {
+        const element = document.getElementById("budget-to-print");
+
+        if (!element) {
+            console.error("El elemento con id 'budget-to-print' no existe.");
+            return;
+        }
+
+        try {
+            // Clonamos el contenido del elemento para agregar estilos personalizados
+            const clonedElement = element.cloneNode(true);
+            clonedElement.style.position = "absolute";
+            clonedElement.style.top = "0px";
+            clonedElement.style.left = "-9999px";
+            clonedElement.style.borderRadius = "0px";
+            clonedElement.style.border = "none";
+
+            // Aplicar estilos específicos para el PDF
+            clonedElement.style.fontSize = "14px";
+            clonedElement.style.color = "#333";
+            clonedElement.querySelectorAll("table").forEach((table) => {
+                table.style.borderCollapse = "collapse";
+                table.style.fontSize = "14px";
+            });
+            clonedElement
+                .querySelectorAll("td, th, thead, tbody, tr")
+                .forEach((cell) => {
+                    cell.style.border = "0.5px solid #242424";
+                });
+            clonedElement.querySelectorAll("td,th").forEach((cell) => {
+                cell.style.margin = "20px 0px 20px 0px ";
+                cell.style.paddingBottom = "20px"; // Eliminar padding
+                cell.style.paddingTop = "10px"; // Eliminar padding
+            });
+            clonedElement.querySelectorAll("th").forEach((parrafo) => {
+                parrafo.style.paddingBottom = "12px"; // Eliminar padding
+                parrafo.style.paddingTop = "2px"; // Eliminar padding
+                parrafo.style.fontSize = "14px";
+            });
+            // Ajustar párrafos
+            clonedElement.querySelectorAll("td p").forEach((parrafo) => {
+                parrafo.style.margin = "0px 50px 0px 0px "; // Eliminar márgenes
+                parrafo.style.padding = "0px 50px 0px 0px "; // Eliminar padding
+                parrafo.style.lineHeight = "1.5"; // Ajustar interlineado para espacio entre líneas
+                parrafo.style.display = "block"; // Asegurar que el párrafo se comporte como un bloque
+                parrafo.style.width = "100%"; // Asegurar que ocupe todo el ancho del contenedor
+                parrafo.style.textAlign = "left"; // Alineación horizontal (opcional)
+            });
+            clonedElement.querySelectorAll("img").forEach((img) => {
+                img.style.width = "800px"; // Ajusta según el tamaño real deseado
+                img.style.height = "auto"; // Mantiene la proporción
+                img.style.objectFit = "contain"; // Asegura que la imagen se ajuste sin recortarse
+            });
+            clonedElement
+                .querySelectorAll("#intro > div")
+                .forEach((parrafo) => {
+                    parrafo.style.paddingBottom = "16px"; // Eliminar padding
+                });
+            clonedElement
+                .querySelectorAll("#finalTable td")
+                .forEach((parrafo) => {
+                    parrafo.style.paddingBottom = "12px"; // Eliminar padding
+                    parrafo.style.paddingTop = "2px"; // Eliminar padding
+                    parrafo.style.fontSize = "14px";
+                });
+
+            // Insertar el clon en el DOM
+            document.body.appendChild(clonedElement);
+
+            // Generar el canvas usando el clon con estilos personalizados
+            const canvas = await html2canvas(clonedElement, {
+                scale: 2, // Mejor calidad
+                useCORS: true,
+            });
+
+            // Eliminar el clon después de capturar el canvas
+            document.body.removeChild(clonedElement);
+
+            // Convertimos el canvas en una imagen PNG
+            const imgData = canvas.toDataURL("image/png");
+
+            // Crear el PDF
+            const pdf = new jsPDF("portrait", "mm", "a4");
+            const pdfWidth = 210; // Ancho en mm para A4
+            const pdfHeight = (canvas.height * pdfWidth) / canvas.width; // Altura proporcional
+
+            pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
+
+            // Abrir en nueva pestaña
+            const pdfBlobUrl = pdf.output("bloburl");
+            window.open(pdfBlobUrl, "_blank");
+        } catch (error) {
+            console.error("Error al generar el PDF:", error);
+        }
+    };
     return (
         <>
             <div className="pb-8 px-16 bg-white min-h-screen">
@@ -66,6 +164,12 @@ function BudgetDetails() {
                         </Link>
                     </div>
                 </div>
+                <button
+                    onClick={handleGeneratePDF}
+                    className="bg-[#9C846A] text-white px-4 py-2 rounded-lg shadow-md hover:bg-[#8a795e] focus:outline-none"
+                >
+                    Generar PDF
+                </button>
                 {loader ? (
                     <div className="overflow-x-auto my-8 flex justify-center items-center h-[100px]">
                         <Grid
@@ -80,11 +184,14 @@ function BudgetDetails() {
                         />
                     </div>
                 ) : (
-                    <div className="border border-gray-300 p-8 shadow-lg rounded-lg max-w-6xl m-auto">
+                    <div
+                        id="budget-to-print"
+                        className="border border-gray-300 p-8 shadow-lg rounded-lg max-w-6xl m-auto"
+                    >
                         <div className="text-center w-[240px] m-auto mt-4 mb-6">
                             <img
                                 className=""
-                                src="./../logo-dacar.svg"
+                                src="./../logo-dacar.png"
                                 alt="Logo Dacar"
                             />
                         </div>
@@ -99,7 +206,10 @@ function BudgetDetails() {
                                 <li>WHATSAPP: 11.5019.9244</li>
                             </ul>
                         </div>
-                        <div className="flex justify-between mb-4 py-4 mt-4 border-t-4  border-t-[#9C846A] border-b-4  border-b-[#9C846A]">
+                        <div
+                            className="flex justify-between mb-4 py-4 mt-4 border-t-4  border-t-[#9C846A] border-b-4  border-b-[#9C846A]"
+                            id="intro"
+                        >
                             <div className="flex flex-col gap-0">
                                 <p className="text-gray-600">
                                     <span className="font-bold">CLIENTE:</span>{" "}
@@ -155,12 +265,12 @@ function BudgetDetails() {
                                 {budget.furniture?.map((furn, idx) => (
                                     <tr
                                         key={idx}
-                                        className=" text-center border-b-2 border-gray-700"
+                                        className=" text-left border-b-2 border-gray-700"
                                     >
                                         <td className="px-2 py-4 whitespace-nowrap text-sm align-top text-gray-700 ">
                                             {furn.name}
                                         </td>
-                                        <td className="px-2 py-4 text-center whitespace-nowrap text-sm align-top text-gray-700 border-r-2 border-r-black border-l border-l-black">
+                                        <td className="px-2 py-4 text-left whitespace-nowrap text-sm align-top text-gray-700 border-r-2 border-r-black border-l-2 border-l-black">
                                             <div className="w-fit max-w-[500px] m-auto text-left">
                                                 <p className="mb-2 break-words whitespace-normal">
                                                     {budget.comments
@@ -182,7 +292,7 @@ function BudgetDetails() {
                                         <td className="px-2 py-4 whitespace-nowrap text-sm align-middel text-gray-[#9C846A]  border-r-2 border-r-black">
                                             DETALLES DE MÓDULOS
                                         </td>
-                                        <td colSpan={3} className="px-8 py-4">
+                                        <td colSpan={3} className="px-6 py-4">
                                             {furn.category ||
                                             (furn.width &&
                                                 furn.height &&
@@ -254,7 +364,7 @@ function BudgetDetails() {
                                     </td>
                                     <td
                                         colSpan={2}
-                                        className="uppercase px-2 py-4 text-left whitespace-nowrap text-sm align-midd  border-r-2 border-r-black border-l-2 border-l-black"
+                                        className="uppercase px-6 py-4 text-left whitespace-nowrap text-sm align-midd  border-r-2 border-r-black border-l-2 border-l-black"
                                     >
                                         <strong>
                                             {budget.placement && budget.shipment
@@ -267,14 +377,14 @@ function BudgetDetails() {
                                         </strong>
                                     </td>
                                 </tr>
-               
+
                                 <tr className=" text-center border-b-2 border-gray-700 text-gray-700">
                                     <td className="px-2 py-4 text-left whitespace-nowrap text-sm align-midd  border-r-2 border-r-black border-l-2 border-l-black">
                                         FORMA DE PAGO
                                     </td>
                                     <td
                                         colSpan={2}
-                                        className="uppercase px-2 py-4 text-left whitespace-nowrap text-sm align-midd  border-r-2 border-r-black border-l-2 border-l-black"
+                                        className="uppercase px-6 py-4 text-left whitespace-nowrap text-sm align-midd  border-r-2 border-r-black border-l-2 border-l-black"
                                     >
                                         <p>
                                             50% SEÑA. 50% SALDO PARA COORDINAR
@@ -293,7 +403,7 @@ function BudgetDetails() {
                                     </td>
                                     <td
                                         colSpan={2}
-                                        className="uppercase px-2 py-4 text-left whitespace-nowrap text-sm align-midd  border-r-2 border-r-black border-l-2 border-l-black"
+                                        className="uppercase px-6 py-4 text-left whitespace-nowrap text-sm align-midd  border-r-2 border-r-black border-l-2 border-l-black"
                                     >
                                         Transferencia, Depósito, Efectivo
                                     </td>
@@ -304,7 +414,7 @@ function BudgetDetails() {
                                     </td>
                                     <td
                                         colSpan={2}
-                                        className="uppercase px-2 py-4 text-left whitespace-normal text-sm align-midd  border-r-2 border-r-black border-l-2 border-l-black"
+                                        className="uppercase px-6 py-4 text-left whitespace-normal text-sm align-midd  border-r-2 border-r-black border-l-2 border-l-black"
                                     >
                                         TIEMPO DE PRODUCCIÓN: 90 DÍAS A PARTIR
                                         DE LA APROBACIÓN Y FIRMA DE PLANOS. NO
@@ -322,7 +432,7 @@ function BudgetDetails() {
 
                         <div className="border-t-4  border-t-[#9C846A] border-b-4  border-b-[#9C846A] pt-6 mt-6 pb-6">
                             <div className="w-full flex flex-col ">
-                                <table>
+                                <table id="finalTable">
                                     <tbody>
                                         <tr className="bg-[#9C846A] text-white border border-gray-700">
                                             <td className="w-3/4 px-6 text-md py-1 border-r border-gray-600">
